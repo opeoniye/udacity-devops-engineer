4acf3cb9fd77a318668fd73db16ade4c503e8411
Samuel John <opeoniye@gmai.com>
2022-08-20 19:24:04 +0100

    finishing project tasks

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="opeoniye/predict-api:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+echo "Docker Image: $dockerpath"
+kubectl run predict-api \
+    --image=$dockerpath \
+    --port=80 --labels app=predict-api
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predict-api 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="opeoniye/predict-api:v1.0"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ~/my_password.txt | docker login -u opeoniye --password-stdin
+docker tag devops $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
